25c7550dcd6f7d1d7408d738fbca27f8e13b07c8
phongnguyenangelhack <phong.nguyen@angelhack.com>
2024-08-02 16:53:14 +0700

    format code

--- src/helpers/pagination.helper.ts
+++ src/helpers/pagination.helper.ts
@@ -1,23 +1,21 @@
-import { NumberField } from './../decorators';
+import { NumberField } from "./../decorators";
 
 export class PaginationDto {
-  @NumberField({ description: 'page', example: 1 })
+  @NumberField({ description: "page", example: 1 })
   page: number;
 
-  @NumberField({ description: 'pageSize', example: 10 })
+  @NumberField({ description: "pageSize", example: 10 })
   pageSize: number;
 }
-export class PaginationHelper <T>{
-    constructor(data:T[],total: number,page:number,pageSize:number){
-        this.data=data;
-        this.total = total;
-        this.page = page;
-        this.totalPage = Math.ceil(total / pageSize);
-    }
-    private data:T[];
-    private total: number;
-    private page: number;
-    private pageSize: number;
-    private totalPage:number;
-  
+export class PaginationHelper<T> {
+  constructor(
+    public readonly data: T[],
+    public readonly total: number,
+    public readonly page: number,
+    public readonly pageSize: number
+  ) {
+    this.totalPage = Math.ceil(total / pageSize);
+  }
+
+  private readonly totalPage: number;
 }
