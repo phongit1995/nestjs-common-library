cff9e905b5b8dcbb45f86a0e52a391caf47e35ce
phongnguyenangelhack <phong.nguyen@angelhack.com>
2024-08-02 16:57:31 +0700

    update interceptor

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './decorators';
 export * from './helpers';
+export * from './interceptors';
---
+++ src/interceptors/response.interceptor.ts
@@ -0,0 +1,47 @@
+import {
+    CallHandler,
+    ExecutionContext,
+    HttpException,
+    Injectable,
+    NestInterceptor,
+  } from '@nestjs/common';
+  import { catchError, Observable, throwError } from 'rxjs';
+  import { map } from 'rxjs/operators';
+  
+  @Injectable()
+  export class ResponseSuccessInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+      return next.handle().pipe(
+        map((data) => {
+          if (data == undefined) return null;
+          const statusCode = data.statusCode;
+          const message = data.message;
+          if (statusCode != null) delete data.statusCode;
+          if (message != null) delete data.message;
+          return {
+            code: context.switchToHttp().getResponse().statusCode || 200,
+            message: message ? message : 'success',
+            data,
+          };
+        }),
+      );
+    }
+  }
+  
+  export class ResponseErrorInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+      return next.handle().pipe(
+        catchError((err) => {
+          if (err instanceof HttpException) {
+            if (err.getResponse() instanceof String) {
+              return throwError(() => ({
+                statusCode: err.getStatus(),
+                message: err.getResponse(),
+              }));
+            } else return throwError(() => err);
+          }
+          return throwError(() => err);
+        }),
+      );
+    }
+  }
