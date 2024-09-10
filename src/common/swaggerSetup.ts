279cff0ba921b005ef14cb194b2bfef0b54e395b
phong <phongit1995@gmail.com>
2024-09-10 08:10:00 +0700

    update swagger setup

--- src/common/swaggerSetup.ts
+++ src/common/swaggerSetup.ts
@@ -28,6 +28,22 @@ export function setUpSwagger(
       persistAuthorization: config.persistAuthorization ? true : false,
     },
   };
+  const vercelConfig = {
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
+  };
+  if (process.env.ENV == "VERCEL") {
+    Object.assign(customOptions, vercelConfig);
+  }
+  
   SwaggerModule.setup(
     config?.url ? config?.url : "swagger",
     app,
